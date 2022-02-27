9211ea9dd3245159a73d16992d515196d30e8f55
Demian <demianpoggio@gmail.com>
2022-02-27 01:42:55 -0300

    Se agrega configuracion de NGINX

--- src/routers/routerRandoms.js
+++ src/routers/routerRandoms.js
@@ -1,14 +1,16 @@
 const { Router } = require('express')
 const { fork } = require('child_process')
+const Log = require('../lib/log.js')
 
 const routerRandoms = Router();
 
 /**** Rutas ****/
 routerRandoms.get('/', (req, res, next) => {  
+    Log.info('Consulta a /api/randoms')
     try {
         const cant = req.query.cant
 
-        const randomController = fork('./src/controllers/randoms.js')
+        const randomController = fork('./controllers/randoms.js')
         randomController.send({ cant })
         randomController.on('message', listaRandom => {
             res.json(listaRandom)
